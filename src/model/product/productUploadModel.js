68dbbf88d453aa257fc319573ac0acbf8fd72648
pritam sarkar <pritam7796419792@gmail.com>
2024-09-13 16:57:33 +0530

    aaaa

--- src/model/product/productUploadModel.js
+++ src/model/product/productUploadModel.js
@@ -1,33 +1,36 @@
 import mongoose from "mongoose";
-const productSchema = new mongoose.Schema({
-  ProductName: {
-    type: String,
-    required: true,
+const productSchema = new mongoose.Schema(
+  {
+    ProductName: {
+      type: String,
+      required: true,
+    },
+    ProductPrice: {
+      type: Number,
+      required: true,
+    },
+    ProductCategory: {
+      type: String,
+      required: true,
+    },
+    ProductLength: {
+      type: String,
+      required: true,
+    },
+    ProductWidth: {
+      type: String,
+      required: true,
+    },
+    ProductDescription: {
+      type: String,
+      required: true,
+    },
+    uploadedImageURLs: {
+      type: Array,
+      required: true,
+    },
   },
-  ProductPrice: {
-    type: Number,
-    required: true,
-  },
-  ProductCategory: {
-    type: String,
-    required: true,
-  },
-  ProductLength: {
-    type: String,
-    required: true,
-  },
-  ProductWidth: {
-    type: String,
-    required: true,
-  },
-  ProductDescription: {
-    type: String,
-    required: true,
-  },
-  uploadedImageURLs: {
-    type: Array,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 const Product = mongoose.model("Product", productSchema);
 export default Product;
