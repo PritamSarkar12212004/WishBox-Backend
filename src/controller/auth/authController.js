87599bbf4aedf806e60187c58a2dd33df519320c
pritam sarkar <pritam7796419792@gmail.com>
2024-09-23 22:50:34 +0530

    error set

--- src/controller/auth/authController.js
+++ src/controller/auth/authController.js
@@ -2,13 +2,10 @@ import User from "../../model/user/userModel.js";
 const authAdminController = async (req, res) => {
   const data = await req.body;
   const response = await User.findOne(data);
-
-  if (
-    response.phoneNumber == process.env.ADMIN_PHONE &&
-    response.password == process.env.ADMIN_PASSWORD
-  ) {
+  const fianlData = (await response.phoneNumber) == process.env.ADMIN_PHONE;
+  const finalDatas = (await response.password) == process.env.ADMIN_PASSWORD;
+  if (fianlData && finalDatas) {
     res.send(true);
-
   } else {
     res.send(false);
   }
