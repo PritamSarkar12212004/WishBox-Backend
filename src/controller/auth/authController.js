f7be773a0848a555ae0418b95c8637431d32b21b
pritam sarkar <pritam7796419792@gmail.com>
2024-09-23 22:29:38 +0530

    ok

--- src/controller/auth/authController.js
+++ src/controller/auth/authController.js
@@ -2,12 +2,13 @@ import User from "../../model/user/userModel.js";
 const authAdminController = async (req, res) => {
   const data = await req.body;
   const response = await User.findOne(data);
-  
+
   if (
     response.phoneNumber == process.env.ADMIN_PHONE &&
-    response.password === process.env.ADMIN_PASSWORD
+    response.password == process.env.ADMIN_PASSWORD
   ) {
     res.send(true);
+
   } else {
     res.send(false);
   }
